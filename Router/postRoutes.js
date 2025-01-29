ee970e8385cb67298569d194f30687d9fe41fb38
Your Name <your_email@example.com>
2025-01-29 10:46:33 +0530

    Add add new think

--- Router/postRoutes.js
+++ Router/postRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const {getImages} = require('../Controllers/PostController')
+const {getImages,postComment,getComment} = require('../Controllers/PostController')
 const router = express.Router();
 
 
+router.post('/postComment',postComment)
+router.get('/getComment',getComment)
 router.get('/images',getImages)
 
 module.exports = router
